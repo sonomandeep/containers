02b0b195be0f59f3283f7f4dfccb5b126456cc8b
Mandeep Singh <hello@mando.sh>
2025-10-22 10:45:11 +0000

    feat: setup containers table

--- apps/web/app/(platform)/containers/page.tsx
+++ apps/web/app/(platform)/containers/page.tsx
@@ -1,3 +1,35 @@
-export default function Page() {
-  return <div>Hello World</div>;
+import type { Container } from "@/components/containers/table/columns";
+import { columns } from "@/components/containers/table/columns";
+import { ContainersTable } from "@/components/containers/table/data-table";
+
+async function getData(): Promise<Array<Container>> {
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ];
+}
+
+export default async function Page() {
+  const data = await getData();
+  return (
+    <div>
+      <ContainersTable columns={columns} data={data} />
+    </div>
+  );
 }
---
+++ apps/web/components/containers/table/data-table.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import type { ColumnDef } from "@tanstack/react-table";
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import { ArrowUpDownIcon, FunnelIcon } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function ContainersTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="overflow-hidden rounded-md">
+      <div className="w-full inline-flex items-center px-2 pb-2 justify-between border-b">
+        <h2>All Containers</h2>
+
+        <div className="inline-flex items-center gap-4">
+          <FunnelIcon className="size-3.5 opacity-60" />
+          <ArrowUpDownIcon className="size-3.5 opacity-60" />
+        </div>
+      </div>
+
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
