00256dfd2c07e17b12358d23440eaef2d1617575
Mandeep Singh <sonomandeep@gmail.com>
2025-10-22 08:01:51 +0000

    feat: update eslint config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,1 +1,12 @@
+import antfu from "@antfu/eslint-config";
 
+export default antfu({
+  stylistic: {
+    indent: 2,
+    jsx: true,
+    quotes: "double",
+    semi: true,
+  },
+  formatters: true,
+  react: true,
+});
