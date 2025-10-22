5e16235468557b6f7ce5122ef1de2899181a83da
Mandeep Singh <sonomandeep@gmail.com>
2025-10-22 09:03:55 +0000

    feat: add shadcn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
-    <main className="h-full w-full flex items-center justify-center">
+    <main className="h-full w-full flex items-center justify-center gap-2">
       Hello World
+      <Button>Click Me!</Button>
     </main>
   );
 }
