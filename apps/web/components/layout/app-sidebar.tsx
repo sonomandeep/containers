29473890eb4c34491e04ad69a3492be29a187877
Mandeep Singh <hello@mando.sh>
2025-11-04 23:52:50 +0000

    feat: refactor sidebar

--- apps/web/components/layout/app-sidebar.tsx
+++ apps/web/components/layout/app-sidebar.tsx
@@ -1,41 +1,39 @@
+"use client";
+
 import {
   BoxIcon,
-  ChevronDownIcon,
   HardDriveIcon,
   LayoutDashboardIcon,
   ServerIcon,
 } from "lucide-react";
-
+import Image from "next/image";
 import {
   Sidebar,
   SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
+  SidebarFooter,
   SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { NavMain } from "./nav-main";
+import NavUser from "./nav-user";
 
 const items = [
   {
-    title: "Home",
+    name: "Dashboard",
     url: "/",
     icon: LayoutDashboardIcon,
   },
   {
-    title: "Containers",
+    name: "Containers",
     url: "/containers",
     icon: BoxIcon,
   },
   {
-    title: "Images",
+    name: "Images",
     url: "/images",
     icon: ServerIcon,
   },
   {
-    title: "Volumes",
+    name: "Volumes",
     url: "/volumes",
     icon: HardDriveIcon,
   },
@@ -45,37 +43,25 @@ export function AppSidebar() {
   return (
     <Sidebar variant="inset">
       <SidebarHeader>
-        <div className="inline-flex items-center justify-between">
-          <div className="inline-flex items-center gap-3">
-            <div className="bg-neutral-200 size-8 rounded-md"></div>
-            <h1>mando.sh</h1>
-          </div>
-
-          <ChevronDownIcon className="opacity-60 size-3.5" />
+        <div className="inline-flex items-center gap-3">
+          <Image src="/logo.svg" alt="ACME Inc. logo" width={24} height={24} />
+          <h1>ACME Inc.</h1>
         </div>
       </SidebarHeader>
 
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Platform</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => {
-                return (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
-                      <a href={item.url}>
-                        <item.icon />
-                        <span>{item.title}</span>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                );
-              })}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <NavMain items={items} />
       </SidebarContent>
+
+      <SidebarFooter>
+        <NavUser
+          user={{
+            name: "Mando",
+            email: "hello@mando.sh",
+            avatar: "https://github.com/sonomandeep.png",
+          }}
+        />
+      </SidebarFooter>
     </Sidebar>
   );
 }
---
+++ apps/web/components/layout/nav-main.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import type { LucideIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  items: Array<{
+    name: string;
+    url: string;
+    icon: LucideIcon;
+  }>;
+}
+
+export function NavMain({ items }: Props) {
+  const pathname = usePathname();
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Platform</SidebarGroupLabel>
+
+      <SidebarMenu>
+        {items.map((item) => (
+          <SidebarMenuItem key={item.name}>
+            <SidebarMenuButton asChild>
+              <Link
+                href={item.url}
+                className={cn(
+                  pathname === item.url && "bg-sidebar-accent text-sidebar-accent-foreground",
+                )}
+              >
+                <item.icon />
+                <span>{item.name}</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
