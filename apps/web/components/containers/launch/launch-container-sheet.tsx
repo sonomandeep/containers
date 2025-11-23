620ffe6d216b28ba70dce16ab0deec9acaa7244f
Mandeep Singh <hello@mando.sh>
2025-11-23 22:02:53 +0000

    feat: add field component

--- apps/web/components/containers/launch/launch-container-sheet.tsx
+++ apps/web/components/containers/launch/launch-container-sheet.tsx
@@ -8,10 +8,15 @@ import {
 } from "@/components/core/sheet-header";
 import { Button } from "@/components/ui/button";
 import {
-  Sheet,
-  SheetContent,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+} from "@/components/ui/field";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 export function LaunchContainerSheet() {
   return (
@@ -31,7 +36,34 @@ export function LaunchContainerSheet() {
           </div>
         </SheetHeaderToolbar>
 
-        <div className="p-4">Hello World</div>
+        <form className="p-4">
+          <FieldSet>
+            <FieldLegend>Profile</FieldLegend>
+            <FieldDescription>
+              This appears on invoices and emails.
+            </FieldDescription>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="name">Full name</FieldLabel>
+                <Input id="name" autoComplete="off" placeholder="Evil Rabbit" />
+                <FieldDescription>
+                  This appears on invoices and emails.
+                </FieldDescription>
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="username">Username</FieldLabel>
+                <Input id="username" autoComplete="off" aria-invalid />
+                <FieldError>Choose another username.</FieldError>
+              </Field>
+              <Field orientation="horizontal">
+                <Switch id="newsletter" />
+                <FieldLabel htmlFor="newsletter">
+                  Subscribe to the newsletter
+                </FieldLabel>
+              </Field>
+            </FieldGroup>
+          </FieldSet>
+        </form>
       </SheetContent>
     </Sheet>
   );
