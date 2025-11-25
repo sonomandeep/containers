5cacee72269025413ca0836b21f246e7c2f4d0cb
Mandeep Singh <hello@mando.sh>
2025-11-25 23:13:30 +0000

    fix: web logger.server

--- apps/web/lib/logger/logger.server.ts
+++ apps/web/lib/logger/logger.server.ts
@@ -1,18 +1,17 @@
 // lib/logger.server.ts
 import pino from "pino";
+import pinoPretty from "pino-pretty";
 import env from "@/lib/env";
 
-export const logger = pino({
-  level: env.NODE_ENV === "production" ? "info" : "debug",
-  transport:
-    env.NODE_ENV !== "production"
-      ? {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-            translateTime: "HH:MM:ss",
-            ignore: "pid,hostname",
-          },
-        }
-      : undefined,
-});
+const isDev = env.NODE_ENV !== "production";
+
+export const logger = isDev
+  ? pino(
+      { level: "debug" },
+      pinoPretty({
+        colorize: true,
+        translateTime: "HH:MM:ss",
+        ignore: "pid,hostname",
+      }),
+    )
+  : pino({ level: isDev ? "debug" : "info" });
