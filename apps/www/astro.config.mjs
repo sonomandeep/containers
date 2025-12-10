79b2ec99eea2b4b59585f9b2e52541a20119fe5a
Mandeep Singh <hello@mando.sh>
2025-12-10 16:15:20 +0000

    feat: update header button with icon

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 
+import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
