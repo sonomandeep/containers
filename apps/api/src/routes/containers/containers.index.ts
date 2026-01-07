6673d31fc90dd397cf64972f9b2737e95226fd50
Mandeep Singh <hello@mando.sh>
2026-01-07 13:26:52 +0100

    fix(api): add cors allow localhost

--- apps/api/src/routes/containers/containers.index.ts
+++ apps/api/src/routes/containers/containers.index.ts
@@ -1,7 +1,12 @@
 import { createRouter } from "@/lib/create-app";
-
+import { cors } from "hono/cors";
 import * as handlers from "./containers.handlers";
 import * as routes from "./containers.routes";
+
+const allowedOrigins = new Set([
+  "http://core.internal:3000",
+  "http://localhost:3000",
+]);
 
 const router = createRouter()
   .openapi(routes.list, handlers.list)
@@ -9,6 +14,20 @@ const router = createRouter()
   .openapi(routes.remove, handlers.remove)
   .openapi(routes.stop, handlers.stop)
   .openapi(routes.start, handlers.start)
-  .get(routes.metrics.path, handlers.metrics);
+  .get(
+    routes.metrics.path,
+    cors({
+      origin: (origin) => {
+        if (!origin) {
+          return "*";
+        }
+
+        return allowedOrigins.has(origin) ? origin : null;
+      },
+      // per ora niente cookie -> false
+      credentials: false,
+    }),
+    handlers.metrics
+  );
 
 export default router;
