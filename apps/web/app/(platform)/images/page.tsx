5d528b6dbfa91e48a5da304ee55e3f46c3d8f225
Mandeep Singh <hello@mando.sh>
2026-01-12 17:27:10 +0100

    feat(web): add metrics section in images page

--- apps/web/app/(platform)/images/page.tsx
+++ apps/web/app/(platform)/images/page.tsx
@@ -1,4 +1,10 @@
-import { HardDriveIcon, LayersIcon, MemoryStickIcon } from "lucide-react";
+import {
+  AlertTriangleIcon,
+  ArchiveIcon,
+  HardDriveIcon,
+  LayersIcon,
+  MemoryStickIcon,
+} from "lucide-react";
 import {
   SectionCard,
   SectionCardDescription,
@@ -8,6 +14,15 @@ import {
   SectionCardTitle,
 } from "@/components/core/section-card";
 import { SegmentedProgressBar } from "@/components/core/segmented-progress-bar";
+import {
+  MetricCard,
+  MetricCardContent,
+  MetricCardHeader,
+  MetricCardLabel,
+  MetricCardProgress,
+  MetricCardStats,
+  MetricCardValue,
+} from "@/components/core/metric-card";
 
 export default function Page() {
   return (
@@ -18,75 +33,64 @@ export default function Page() {
             <SectionCardTitle>Cluster Images</SectionCardTitle>
             <SectionCardDescription>37 images</SectionCardDescription>
           </SectionCardHeaderContent>
+
           <SectionCardMeta>376 GB</SectionCardMeta>
         </SectionCardHeader>
 
         <div className="grid grid-cols-3 gap-4">
-          <div className="flex flex-col gap-3 rounded-lg border border-neutral-100 bg-neutral-50 p-3">
-            <div className="inline-flex items-center gap-1">
-              <LayersIcon className="size-3 text-muted-foreground" />
-              <span className="font-medium text-muted-foreground text-xs">
-                CPU
-              </span>
-            </div>
+          <MetricCard>
+            <MetricCardHeader>
+              <LayersIcon />
+              In Use
+            </MetricCardHeader>
+            <MetricCardContent>
+              <MetricCardStats>
+                <MetricCardValue>16</MetricCardValue>
+                <MetricCardLabel>224 GB</MetricCardLabel>
+              </MetricCardStats>
+              <MetricCardProgress>
+                <SegmentedProgressBar value={59.25} />
+              </MetricCardProgress>
+            </MetricCardContent>
+          </MetricCard>
 
-            <div className="flex w-full flex-col gap-1">
-              <div className="inline-flex w-full items-center justify-between font-mono">
-                <span className="font-medium text-neutral-700">32.5%</span>
-                <span className="font-medium text-muted-foreground text-xs">
-                  12 vCPU
-                </span>
-              </div>
+          <MetricCard>
+            <MetricCardHeader>
+              <ArchiveIcon />
+              Unused
+            </MetricCardHeader>
+            <MetricCardContent>
+              <MetricCardStats>
+                <MetricCardValue>7</MetricCardValue>
+                <MetricCardLabel>98 GB</MetricCardLabel>
+              </MetricCardStats>
+              <MetricCardProgress>
+                <SegmentedProgressBar
+                  activeClassName="bg-amber-500"
+                  value={25.92}
+                />
+              </MetricCardProgress>
+            </MetricCardContent>
+          </MetricCard>
 
-              <div className="h-3 w-full">
-                <SegmentedProgressBar value={32.5} />
-              </div>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-3 rounded-lg border border-neutral-100 bg-neutral-50 p-3">
-            <div className="inline-flex items-center gap-1">
-              <MemoryStickIcon className="size-3 text-muted-foreground" />
-              <span className="font-medium text-muted-foreground text-xs">
-                Memory
-              </span>
-            </div>
-
-            <div className="flex w-full flex-col gap-1">
-              <div className="inline-flex w-full items-center justify-between font-mono">
-                <span className="font-medium text-neutral-700">93.8%</span>
-                <span className="font-medium text-muted-foreground text-xs">
-                  32 GB
-                </span>
-              </div>
-
-              <div className="h-3 w-full">
-                <SegmentedProgressBar value={93.8} />
-              </div>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-3 rounded-lg border border-neutral-100 bg-neutral-50 p-3">
-            <div className="inline-flex items-center gap-1">
-              <HardDriveIcon className="size-3 text-muted-foreground" />
-              <span className="font-medium text-muted-foreground text-xs">
-                Disk
-              </span>
-            </div>
-
-            <div className="flex w-full flex-col gap-1">
-              <div className="inline-flex w-full items-center justify-between font-mono">
-                <span className="font-medium text-neutral-700">74.2%</span>
-                <span className="font-medium text-muted-foreground text-xs">
-                  512 GB
-                </span>
-              </div>
-
-              <div className="h-3 w-full">
-                <SegmentedProgressBar value={74.2} />
-              </div>
-            </div>
-          </div>
+          <MetricCard>
+            <MetricCardHeader>
+              <AlertTriangleIcon />
+              Dangling
+            </MetricCardHeader>
+            <MetricCardContent>
+              <MetricCardStats>
+                <MetricCardValue>4</MetricCardValue>
+                <MetricCardLabel>56 GB</MetricCardLabel>
+              </MetricCardStats>
+              <MetricCardProgress>
+                <SegmentedProgressBar
+                  activeClassName="bg-red-500"
+                  value={14.81}
+                />
+              </MetricCardProgress>
+            </MetricCardContent>
+          </MetricCard>
         </div>
       </SectionCard>
     </div>
