3e5c7551109cafee951c582dbe273b2dda0685cd
Mandeep Singh <hello@mando.sh>
2026-01-12 17:43:41 +0100

    feat(web): update images page layout

--- apps/web/app/(platform)/images/page.tsx
+++ apps/web/app/(platform)/images/page.tsx
@@ -1,9 +1,9 @@
 import {
   AlertTriangleIcon,
   ArchiveIcon,
-  HardDriveIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
   LayersIcon,
-  MemoryStickIcon,
 } from "lucide-react";
 import {
   SectionCard,
@@ -23,6 +23,20 @@ import {
   MetricCardStats,
   MetricCardValue,
 } from "@/components/core/metric-card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardToolbar,
+} from "@/components/core/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
@@ -91,6 +105,50 @@ export default function Page() {
               </MetricCardProgress>
             </MetricCardContent>
           </MetricCard>
+        </div>
+      </SectionCard>
+
+      <SectionCard className="@container min-h-0 flex-1 overflow-hidden">
+        <SectionCardHeader>
+          <SectionCardHeaderContent>
+            <SectionCardTitle>Your Images</SectionCardTitle>
+          </SectionCardHeaderContent>
+        </SectionCardHeader>
+
+        <div className="grid gap-3 flex-1 grid-cols-3">
+          <Card className="flex-1 col-span-2">
+            <CardToolbar>27 images</CardToolbar>
+
+            <CardContent className="flex-1">aaa</CardContent>
+
+            <CardFooter className="justify-between">
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue data-placeholder="Select page size" />
+                </SelectTrigger>
+
+                <SelectContent>
+                  <SelectItem value="page_size_10">10 per page</SelectItem>
+                  <SelectItem value="page_size_20">20 per page</SelectItem>
+                  <SelectItem value="page_size_50">50 per page</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <div className="inline-flex gap-2 items-center">
+                <Button variant="ghost" size="icon-sm">
+                  <ChevronLeftIcon />
+                </Button>
+
+                <span className="font-mono">1</span>
+
+                <Button variant="ghost" size="icon-sm">
+                  <ChevronRightIcon />
+                </Button>
+              </div>
+            </CardFooter>
+          </Card>
+
+          <Card className="h-min">aaa</Card>
         </div>
       </SectionCard>
     </div>
