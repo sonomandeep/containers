04c0214d91f1b5bdd5fffd3ec8920676bcdb2be6
Mandeep Singh <hello@mando.sh>
2026-01-16 09:44:50 +0100

    feat(api): add auth middleware in images router

--- apps/api/src/routes/images/images.index.ts
+++ apps/api/src/routes/images/images.index.ts
@@ -1,8 +1,12 @@
 import { createRouter } from "@/lib/create-app";
+import { authMiddleware } from "@/lib/middlewares/auth.middleware";
 import * as handlers from "./images.handlers";
 import * as routes from "./images.routes";
 
-const router = createRouter()
+const router = createRouter();
+router.use("/images/*", authMiddleware);
+
+router
   .openapi(routes.list, handlers.list)
   .openapi(routes.pull, handlers.pull)
   .openapi(routes.remove, handlers.remove);
