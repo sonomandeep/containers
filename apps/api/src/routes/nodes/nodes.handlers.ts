2b4ea2658e24b35518bc74753691eede4d7fd677
Mandeep Singh <hello@mando.sh>
2026-01-16 09:44:33 +0100

    feat(api): remove unused routes

--- apps/api/src/routes/nodes/nodes.handlers.ts
+++
@@ -1,8 +0,0 @@
-import * as HttpStatusCodes from "stoker/http-status-codes";
-import type { AppRouteHandler } from "@/lib/types";
-import type { Monitor } from "./nodes.routes";
-
-export const monitor: AppRouteHandler<Monitor> = (c) => {
-  c.var.logger.info("hello world!");
-  return c.json({ message: "nodes monitoring" }, HttpStatusCodes.OK);
-};
