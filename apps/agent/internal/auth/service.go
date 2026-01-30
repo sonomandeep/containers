045fa2fb8f4fd8450991d3f45dfd3b8e2ad87258
mando.sh <hello@mando.sh>
2026-01-30 15:57:32 +0100

    feat(agent): add session validation

--- apps/agent/internal/auth/service.go
+++ apps/agent/internal/auth/service.go
@@ -2,10 +2,15 @@
 package auth
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/sonomandeep/containers/agent/internal/config"
 	"go.yaml.in/yaml/v3"
@@ -49,6 +54,15 @@ func GetAuthStatus() (bool, error) {
 	fmt.Println("token:", auth.Token)
 	fmt.Println("expires in:", auth.ExpiresIn)
 
+	isValid, err := auth.getSession()
+	if err != nil {
+		return false, err
+	}
+	if !isValid {
+		// TODO: remove auth file
+		return false, nil
+	}
+
 	return true, nil
 }
 
@@ -70,5 +84,47 @@ func (a auth) validate() error {
 	if a.ExpiresIn <= 0 {
 		return errors.New("invalid expires_in")
 	}
+
 	return nil
+}
+
+func (a auth) getSession() (bool, error) {
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	req, err := http.NewRequest(
+		"GET",
+		"http://paper.sh:9999/api/auth/get-session",
+		nil,
+	)
+	if err != nil {
+		return false, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+a.Token)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return false, fmt.Errorf("status %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
+
+	if strings.TrimSpace(string(body)) == "null" {
+		return false, errors.New("sessione non valida")
+	}
+
+	var tmp map[string]any
+	if err := json.Unmarshal(body, &tmp); err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
