6f07d0bc80ccdf859c3bb20de4c829885ad01a43
mando.sh <hello@mando.sh>
2026-02-10 13:20:52 +0100

    feat(web): add cards

--- apps/web/app/onboarding/page.tsx
+++ apps/web/app/onboarding/page.tsx
@@ -1,11 +1,48 @@
+import { Building2Icon, UsersIcon } from "lucide-react";
 import Image from "next/image";
+import { Logo } from "@/components/core/logo";
 
 export default async function Page() {
   return (
-    <div className="grid h-full w-full grid-cols-3">
-      <div className="col-span-1">hello</div>
+    <div className="grid h-full w-full grid-cols-5">
+      <section className="col-span-2 mx-auto flex w-sm flex-col items-center justify-center gap-6">
+        <header className="inline-flex w-full gap-4">
+          <Logo size={32} />
+          <div className="flex flex-col">
+            <h1>Infrastructure, without the complexity.</h1>
+            <p className="text-muted-foreground text-xs">
+              Create your workspace in seconds or join your team.
+            </p>
+          </div>
+        </header>
 
-      <div className="relative col-span-2 bg-[url(/assets/clouds.jpg)]">
+        <div className="grid grid-cols-2 gap-3">
+          <div className="flex w-full flex-col gap-3 rounded-md border border-card-border bg-card p-3">
+            <UsersIcon className="size-4" />
+
+            <div className="flex flex-col gap-0.5">
+              <h2>Join workspace</h2>
+              <p className="text-muted-foreground text-xs">
+                Enter an invite code or accept an invitation to collaborate with
+                your team.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex w-full flex-col gap-3 rounded-md border border-card-border bg-card p-3">
+            <Building2Icon className="size-4" />
+
+            <div className="flex flex-col gap-0.5">
+              <h2>Create workspace</h2>
+              <p className="text-muted-foreground text-xs">
+                Set up your organization and invite teammates in a few steps.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="relative col-span-3 bg-[url(/assets/clouds.jpg)]">
         <Image
           alt="Paper dashboard"
           className="object-cover object-left py-24 pl-24"
@@ -14,7 +51,7 @@ export default async function Page() {
           sizes="66vw"
           src="/assets/mock.png"
         />
-      </div>
+      </section>
     </div>
   );
 }
