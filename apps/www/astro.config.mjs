68801664daa53543f90f83746f1b386fc5b8276f
Mandeep Singh <sonomandeep@gmail.com>
2026-02-23 21:15:27 +0100

    feat: reset www astro app

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -1,14 +1,5 @@
 // @ts-check
-
-import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [react()],
-});
+export default defineConfig({});
